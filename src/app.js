f37192edd2a834f3412e36f7a87c52fa67354e67
Alexander Kamara <alexander-kamara@outlook.com>
2022-06-24 16:26:42 +0100

    CRUD working

---
+++ src/app.js
@@ -0,0 +1,43 @@
+require("./db/connection")
+const mongoose =require("mongoose");
+const yargs = require("yargs");
+const { addMovie, listMovies, updateMovie, deleteMovie } = require("./movies/moviemethods");
+
+const app = async (yargsObj) => {
+    try {
+        if (yargsObj.add) {
+            await addMovie({ title: yargsObj.title, actor: yargsObj.actor })
+            console.log(await listMovies());
+        } else if (yargsObj.list) {
+            console.log(await listMovies());
+        } else if (yargsObj.update) {
+            const criteria = { title: yargsObj.update };
+            let update = {};
+            if (yargsObj.title) {
+                Object.assign(update, { title: yargsObj.title });
+            }
+            if (yargsObj.actor) {
+                Object.assign(update, { actor: yargsObj.actor });
+            }
+            if (yargsObj.director) {
+                Object.assign(update, { director: yargsObj.director });
+            }
+            if (yargsObj.rating) {
+                Object.assign(update, { rating: yargsObj.rating });
+            }
+            await updateMovie(criteria, update);
+            console.log(await listMovies);
+        } else if (yargsObj.delete) {
+            let criteria = { title: yargsObj.delete };
+            await deleteMovie(criteria);
+            console.log(await listMovies())
+        }else {
+            console.log("Incorrect command");
+        }
+        await mongoose.disconnect();
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+app(yargs.argv);
